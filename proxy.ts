a7a24aaabe2d5257ed1c98333119d11c73519a54
tchappel <thomas.chappel4@gmail.com>
2025-11-11 10:05:21 +0100

    add protected routes and redirect in proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export const proxy = clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+
+export const proxy = clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect({
+      unauthenticatedUrl: `${req.nextUrl.origin}`,
+    });
+  }
+});
 
 export const config = {
   matcher: [
